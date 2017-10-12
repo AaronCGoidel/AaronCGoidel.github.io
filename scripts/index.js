10a17270943de2e00b8546fabdee008382f7adf7
AaronCGoidel <agoidel2019@mka.org>
2017-10-12 13:17:44 -0400

    changed style of links and timing of typing

--- scripts/index.js
+++ scripts/index.js
@@ -204,13 +204,13 @@
 
 function handleTyping(){
     $(".identifier").typed({
-        strings: ["a programmer.", "a writer.", "a creator."],
+        strings: ["a programmer.", "a chef.", "a writer.", "a musician.", "a creator."],
         stringsElement: null,
         typeSpeed: 50,
         startDelay: 200,
-        backSpeed: 0,
+        backSpeed: 30,
         shuffle: false,
-        backDelay: 600,
+        backDelay: 1500,
         loop: true,
         showCursor: true,
         cursorChar: "|",
